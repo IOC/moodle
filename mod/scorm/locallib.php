1f0a9dc8423b7f04dd2349bb059e3b3ac4f37350
Marc Català <reskit@gmail.com>
2013-02-12 11:55:59 +0100

    Incrementa els límits de temps, memòria i timeout del MySQL
    Per a tasques d'administració: cron, backups, actualització, etc.

--- mod/scorm/locallib.php
+++ mod/scorm/locallib.php
@@ -205,6 +205,11 @@ function scorm_get_attemptstatus_array() {
  */
 function scorm_parse($scorm, $full) {
     global $CFG, $DB;
+
+    set_time_limit(3600);
+    raise_memory_limit(MEMORY_EXTRA);
+    $DB->raise_timeout();
+
     $cfg_scorm = get_config('scorm');
 
     if (!isset($scorm->cmid)) {
