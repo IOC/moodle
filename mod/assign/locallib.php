b2cb7c3a8003f93495c292b1411809ed20ae3eeb
Albert Gasset <albert.gasset@gmail.com>
2013-02-27 12:26:16 +0100

    mod/assign: Opció de revertir a esborrany els enviaments en qualificar

--- mod/assign/locallib.php
+++ mod/assign/locallib.php
@@ -5199,6 +5199,15 @@ class assign {
         $mform->addElement('hidden', 'action', 'submitgrade');
         $mform->setType('action', PARAM_ALPHA);
 
+        if ($this->get_instance()->submissiondrafts) {
+            $submission = $this->get_user_submission($userid, false);
+            if ($submission and $submission->status == ASSIGN_SUBMISSION_STATUS_SUBMITTED) {
+                $mform->addElement('checkbox', 'reverttodraft', '',
+                                   get_string('reverttodraftshort', 'assign'));
+                $mform->closeHeaderBefore('reverttodraft');
+            }
+        }
+
         $buttonarray=array();
         $name = get_string('savechanges', 'assign');
         $buttonarray[] = $mform->createElement('submit', 'savegrade', $name);
@@ -5515,6 +5524,15 @@ class assign {
         }
         $this->update_grade($grade);
         $this->notify_grade_modified($grade);
+
+        if (!empty($formdata->reverttodraft)) {
+            $submission = $this->get_user_submission($userid, false);
+            if ($submission) {
+                $submission->status = ASSIGN_SUBMISSION_STATUS_DRAFT;
+                $this->update_submission($submission, false);
+            }
+        }
+
         $user = $DB->get_record('user', array('id' => $userid), '*', MUST_EXIST);
 
         $this->add_to_log('grade submission', $this->format_grade_for_log($grade));
