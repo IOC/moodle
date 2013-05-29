20d488af55accdc6d3eb609661dbb97e2d939bbe
Albert Gasset <albert.gasset@gmail.com>
2013-05-29 13:10:30 +0200

    mod/assign: Permet qualificar "sense qualificació" a les tasques amb escales

--- mod/assign/locallib.php
+++ mod/assign/locallib.php
@@ -1673,6 +1673,9 @@ class assign {
         if ($flags->mailed != 1) {
             $flags->mailed = 0;
         }
+        if ($grade->grade === null) {
+            $flags->mailed = 1;
+        }
 
         return $this->update_user_flags($flags);
     }
@@ -5072,7 +5075,8 @@ class assign {
                     $mform->addHelpButton('gradedisabled', 'gradeoutofhelp', 'assign');
                 }
             } else {
-                $grademenu = array(-1 => get_string("nograde")) + make_grades_menu($this->get_instance()->grade);
+                $grademenu = array(-1 => get_string('nograde'));
+                $grademenu += make_grades_menu($this->get_instance()->grade);
                 if (count($grademenu) > 1) {
                     $gradingelement = $mform->addElement('select', 'grade', get_string('grade') . ':', $grademenu);
 
@@ -5484,6 +5488,9 @@ class assign {
                 // Handle the case when grade is set to No Grade.
                 if (isset($formdata->grade)) {
                     $grade->grade = grade_floatval(unformat_float($formdata->grade));
+                    if ($formdata->grade == -1) {
+                        $grade->grade = null;
+                    }
                 }
             }
         }
