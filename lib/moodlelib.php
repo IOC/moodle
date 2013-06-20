f79d85bbfa223ff389cb7fbfeeef1cbbe4012b24
Albert Gasset <albert.gasset@gmail.com>
2013-06-20 12:10:05 +0200

    Paquets d'idioma inclosos al codi
    Els paquets estan al directori langpack. La instal·lació i
    personalització via web estan desactivades.

--- lib/moodlelib.php
+++ lib/moodlelib.php
@@ -6798,7 +6798,7 @@ class core_string_manager implements string_manager {
             $originalkeys = array_flip($originalkeys);
 
             // and then corresponding local if present and allowed
-            if (!$disablelocal and file_exists("$this->localroot/en_local/$file.php")) {
+            if (!$disablelocal and $this->localroot and file_exists("$this->localroot/en_local/$file.php")) {
                 include("$this->localroot/en_local/$file.php");
             }
             // now loop through all langs in correct order
@@ -6808,7 +6808,7 @@ class core_string_manager implements string_manager {
                 if (file_exists("$this->otherroot/$dep/$file.php")) {
                     include("$this->otherroot/$dep/$file.php");
                 }
-                if (!$disablelocal and file_exists("$this->localroot/{$dep}_local/$file.php")) {
+                if (!$disablelocal and $this->localroot and file_exists("$this->localroot/{$dep}_local/$file.php")) {
                     include("$this->localroot/{$dep}_local/$file.php");
                 }
             }
@@ -6833,7 +6833,7 @@ class core_string_manager implements string_manager {
             $originalkeys = array_keys($string);
             $originalkeys = array_flip($originalkeys);
             // and then corresponding local english if present
-            if (!$disablelocal and file_exists("$this->localroot/en_local/$file.php")) {
+            if (!$disablelocal and $this->localroot and file_exists("$this->localroot/en_local/$file.php")) {
                 include("$this->localroot/en_local/$file.php");
             }
 
@@ -6849,7 +6849,7 @@ class core_string_manager implements string_manager {
                     include("$this->otherroot/$dep/$file.php");
                 }
                 // local customisations
-                if (!$disablelocal and file_exists("$this->localroot/{$dep}_local/$file.php")) {
+                if (!$disablelocal and $this->localroot and file_exists("$this->localroot/{$dep}_local/$file.php")) {
                     include("$this->localroot/{$dep}_local/$file.php");
                 }
             }
