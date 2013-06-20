24820b343b860d58d0ad5d4f991a3e449c58002a
Albert Gasset <albert.gasset@gmail.com>
2013-06-20 12:10:05 +0200

    Paquets d'idioma inclosos al codi
    Els paquets estan al directori langpack. La instal·lació i
    personalització via web estan desactivades.

--- config.php
+++ config.php
@@ -3,5 +3,9 @@
 unset($CFG);
 global $CFG;
 $CFG = new stdClass();
+$CFG->skiplangupgrade = true;
+$CFG->langotherroot = __DIR__ . '/langpacks';
+$CFG->langlocalroot = false;
+
 require_once(__DIR__ . '/../config-moodle2.php');
 require_once(__DIR__ . '/lib/setup.php');
