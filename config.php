c237cd4a41494a8295cf96aec2b0b6880370cbcc
Albert Gasset <albert.gasset@gmail.com>
2013-07-09 12:10:12 +0200

    Configuració  memòra cau
    En comptes d'utilitzar el cache en disc, fem servir l'emmagatzematge
    estàtic (en memòria) per a tots els elements excepte les cadenes
    d'idioma. L'emmagatzematge en disc utilitza el directori configurat
    amb el paràmetre "localcachedir", només és segur utilitzar-lo per a
    les cadenes d'idioma.

--- config.php
+++ config.php
@@ -6,6 +6,7 @@ $CFG = new stdClass();
 $CFG->skiplangupgrade = true;
 $CFG->langotherroot = __DIR__ . '/langpacks';
 $CFG->langlocalroot = false;
+$CFG->altcacheconfigpath = __DIR__  . '/config-muc.php';
 
 require_once(__DIR__ . '/../config-moodle2.php');
 require_once(__DIR__ . '/lib/setup.php');
