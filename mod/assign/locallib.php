1d5555daf0a380c0dbc08051b94f1ef64def66d3
Marc Català <mcatala@itteria.cat>
2015-05-21 16:14:45 +0200

    mod/assign: Fixed word count when portfolio exporting is enabled

--- mod/assign/locallib.php
+++ mod/assign/locallib.php
@@ -2463,6 +2463,7 @@ class assign {
                                                   $submissionid);
         $params = array('overflowdiv' => true, 'context' => $this->get_context());
         $result .= format_text($finaltext, $format, $params);
+        $portfoliohtml = $result;
 
         if ($CFG->enableportfolios && has_capability('mod/assign:exportownsubmission', $this->context)) {
             require_once($CFG->libdir . '/portfoliolib.php');
@@ -2486,9 +2487,9 @@ class assign {
             } else {
                 $button->set_formats(PORTFOLIO_FORMAT_PLAINHTML);
             }
-            $result .= $button->to_html();
+            $portfoliohtml .= $button->to_html();
         }
-        return $result;
+        return array($result, $portfoliohtml);
     }
 
     /**
