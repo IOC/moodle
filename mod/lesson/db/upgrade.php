f83bf747a6f1705888f68dc2419df589a4765de0
Marc Català <mcatala@itteria.cat>
2015-05-22 14:00:37 +0200

    mod/lesson: Configure whether shuffle multiple choice answers

--- mod/lesson/db/upgrade.php
+++ mod/lesson/db/upgrade.php
@@ -75,6 +75,7 @@ function xmldb_lesson_upgrade($oldversion) {
     // Moodle v2.6.0 release upgrade line.
     // Put any upgrade step following this.
 
+
     // Moodle v2.7.0 release upgrade line.
     // Put any upgrade step following this.
 
@@ -134,6 +135,17 @@ function xmldb_lesson_upgrade($oldversion) {
 
         // Lesson savepoint reached.
         upgrade_mod_savepoint(true, 2014111003, 'lesson');
+    }
+
+    {
+        // Define field shuffle to be added to lesson_pages.
+        $table = new xmldb_table('lesson_pages');
+        $field = new xmldb_field('shuffle', XMLDB_TYPE_INTEGER, '3', null, XMLDB_NOTNULL, null, '1', 'contentsformat');
+
+        // Conditionally launch add field shuffle.
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
     }
 
     return true;
