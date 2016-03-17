7a9a235f1d46c695a1c6c2dd78daa4297a22799c
Marc Català <mcatala@itteria.cat>
2016-03-17 12:43:18 +0100

    Hooks per al connector rendiment del lloc (siteperf)

--- lib/outputrenderers.php
+++ lib/outputrenderers.php
@@ -1198,10 +1198,14 @@ class core_renderer extends renderer_base {
             }
         }
 
+        //Admin site performance
+        tool_siteperf::shutdown();
+
         // We always want performance data when running a performance test, even if the user is redirected to another page.
         if (MDL_PERF_TEST && strpos($footer, $this->unique_performance_info_token) === false) {
             $footer = $this->unique_performance_info_token . $footer;
         }
+
         $footer = str_replace($this->unique_performance_info_token, $performanceinfo, $footer);
 
         // Only show notifications when we have a $PAGE context id.
