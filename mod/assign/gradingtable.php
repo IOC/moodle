64384bc9513dda1f692ab7af0886489b9091a184
Marc Català <mcatala@itteria.cat>
2016-03-18 12:26:50 +0100

    mod/assign: Filtre d'esborranys a la llista d'enviaments

--- mod/assign/gradingtable.php
+++ mod/assign/gradingtable.php
@@ -277,6 +277,9 @@ class assign_grading_table extends table_sql implements renderable {
             } else if ($filter == ASSIGN_FILTER_GRANTED_EXTENSION) {
                 $where .= ' AND uf.extensionduedate > 0 ';
 
+            } else if ($filter == ASSIGN_FILTER_DRAFTS) {
+                $where .= ' AND s.status = :draft';
+                $params['draft'] = ASSIGN_SUBMISSION_STATUS_DRAFT;
             } else if (strpos($filter, ASSIGN_FILTER_SINGLE_USER) === 0) {
                 $userfilter = (int) array_pop(explode('=', $filter));
                 $where .= ' AND (u.id = :userid)';
