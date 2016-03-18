d8916d7612bf79e30c9dad4ed0e3bd2b9138ff6a
Marc Català <mcatala@itteria.cat>
2016-03-18 13:09:43 +0100

    mod/lti: LTI activity opened in a new window show its activity page. LTI activity embedded has window scrollbars

--- mod/lti/lib.php
+++ mod/lti/lib.php
@@ -306,7 +306,7 @@ function lti_get_coursemodule_info($coursemodule) {
     $launchcontainer = lti_get_launch_container($lti, $toolconfig);
     if ($launchcontainer == LTI_LAUNCH_CONTAINER_WINDOW) {
         $launchurl = new moodle_url('/mod/lti/launch.php', array('id' => $coursemodule->id));
-        $info->onclick = "window.open('" . $launchurl->out(false) . "', 'lti-".$coursemodule->id."'); return false;";
+        $info->onclick = "window.open('" . $launchurl->out(false) . "', 'lti-".$coursemodule->id."');";
     }
 
     $info->name = $lti->name;
