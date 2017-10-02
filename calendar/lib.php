799ab0a0146e24301fdcfcfc8490fa401f31a8f5
Marc Català <mcatala@itteria.cat>
2017-10-02 17:28:16 +0200

    calendar: New setting to configure the maximum events to be displayed

--- calendar/lib.php
+++ calendar/lib.php
@@ -3294,6 +3294,8 @@ function core_calendar_user_preferences() {
  * @return array $events of selected events or an empty array if there aren't any (or there was an error)
  */
 function calendar_get_legacy_events($tstart, $tend, $users, $groups, $courses, $withduration = true, $ignorehidden = true) {
+    global $CFG;
+
     // Normalise the users, groups and courses parameters so that they are compliant with \core_calendar\local\api::get_events().
     // Existing functions that were using the old calendar_get_events() were passing a mixture of array, int, boolean for these
     // parameters, but with the new API method, only null and arrays are accepted.
@@ -3325,7 +3327,7 @@ function calendar_get_legacy_events($tstart, $tend, $users, $groups, $courses, $
         null,
         null,
         null,
-        40,
+        $CFG->calendar_maxevents_returned,
         null,
         $userparam,
         $groupparam,
